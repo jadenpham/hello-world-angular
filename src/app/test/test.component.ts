905e88c5d5e3e02c6f2f9182ad775d5c8cc41359
jadenpham <tjaden.pham@gmail.com>
2019-10-08 12:25:02 -0700

    second commit with class binding notes added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,13 +4,18 @@ import { Component, OnInit } from '@angular/core';
   //different ways to specify selector, diff syntax on html, 3 ways
   selector: '[app-test]',
   //use template: and ` ` for inline template, instead of .html file
+  //how you normally assign class to a tag
+  //with class binding, using [class]="class name", line 11
+  //using class attribute and class binding at same time, class attribute becomes dummie attribute w class binding, can only use one or the other
+  //can use class binding with boolean, to call it use [class.classname in style] = "classname in component"(boolean), text will show red since hasError = true
+  //can also bind multiple with classes using [ngClass] = "classname"
   template: `
   <h1 class="text-danger"> yoooo what's good {{name}}</h1>
   <h2>{{2+2}}</h2>
-  <h2>{{"Welcome " + name}}</h2>
-  <h2>{{name.length}}</h2>
-  <h2>{{greetUser()}}</h2>
-  <h2>{{"You are on " + site}}</h2>
+  <h2 [class]="successClass">{{"Welcome " + name}}</h2>
+  <h2 class="text-special" [class]="successClass">{{greetUser()}}</h2>
+  <h2 [class.text-danger] = "hasError">{{"You are on " + site}}</h2>
+  <h2 [ngClass] = "messageClasses">Hello World</h2>
   <input [id]="myId" type="text" value="Jaden">
   <input [disabled] = "isDisabled" type="text" value="Jaden">
   `,
@@ -37,6 +42,14 @@ export class TestComponent implements OnInit {
   public myId = "testId";
   //making disabled dynamic w boolean instead of hard coding it, reason to use property instead of interpolation
   public isDisabled = true;
+  public successClass = "text-success";
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError ,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
