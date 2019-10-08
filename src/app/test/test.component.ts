4b6a749e6e025418a872c287b9e1b13283bf349d
jadenpham <tjaden.pham@gmail.com>
2019-10-08 12:51:10 -0700

    commit with style binding notes and example

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,19 +9,29 @@ import { Component, OnInit } from '@angular/core';
   //using class attribute and class binding at same time, class attribute becomes dummie attribute w class binding, can only use one or the other
   //can use class binding with boolean, to call it use [class.classname in style] = "classname in component"(boolean), text will show red since hasError = true
   //can also bind multiple with classes using [ngClass] = "classname"
+  //style binding [style.css property name, ie color] = " 'ie: orange' ";
+  //can also use boolean/conditional expression
   template: `
-  <h1 class="text-danger"> yoooo what's good {{name}}</h1>
+  <!-- <h1 class="text-danger"> yoooo what's good {{name}}</h1> -->
   <h2>{{2+2}}</h2>
-  <h2 [class]="successClass">{{"Welcome " + name}}</h2>
-  <h2 class="text-special" [class]="successClass">{{greetUser()}}</h2>
-  <h2 [class.text-danger] = "hasError">{{"You are on " + site}}</h2>
-  <h2 [ngClass] = "messageClasses">Hello World</h2>
+  <!--<h2 [class]="successClass">{{"Welcome " + name}}</h2>-->
+  <!--<h2 class="text-special" [class]="successClass">{{greetUser()}}</h2>-->
+  <!--<h2 [class.text-danger] = "hasError">{{"You are on " + site}}</h2>-->
+  <!--<h2 [ngClass] = "messageClasses">Hello World</h2>-->
   <input [id]="myId" type="text" value="Jaden">
   <input [disabled] = "isDisabled" type="text" value="Jaden">
+  <h2 [style.color] = "'orange'">Style binding</h2>
+  <!-- so if has error is true, make it red, else green -->
+  <h2 [style.color] = "hasError ?'red' : 'green' ">Style binding w conditional</h2>
+  <!-- can also bind to class attribute so it can change dynamically -->
+  <h2 [style.color] = "highlightColor">Still binding to class attribute</h2>
+  <!-- can style w multiple with ng directive and bind it to object -->
+  <h2 [ngStyle]= "titleStyles">Style binding to an object</h2>
   `,
   // templateUrl: './test.component.html',
   // can also inline template here w `` (backticks)
   // styleUrls: ['./test.component.css']
+  //style binding
   styles: [`
     .text-success{
       color: green;
@@ -29,7 +39,7 @@ import { Component, OnInit } from '@angular/core';
     .text-danger{
       color: red
     }
-    .text-special{
+    .text-special{ 
       font-style: italic;
     }
   `]
@@ -44,11 +54,16 @@ export class TestComponent implements OnInit {
   public isDisabled = true;
   public successClass = "text-success";
   public hasError = true;
+  public highlightColor = "orange";
   public isSpecial = true;
   public messageClasses = {
     "text-success": !this.hasError ,
     "text-danger": this.hasError,
     "text-special": this.isSpecial
+  };
+  public titleStyles ={
+    color: "blue",
+    fontStyle: "italic"
   }
   constructor() { }
 
